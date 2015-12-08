fe2f530d94013bde1a0708d22b597a67f5bc03b5
slaparra <santiago.laparra@atrapalo.com>
2015-12-09 00:28:30 +0100

    search tracks

--- src/Bundle/PlayWithElasticSearchBundle/Controller/ElasticaController.php
+++ src/Bundle/PlayWithElasticSearchBundle/Controller/ElasticaController.php
@@ -4,6 +4,9 @@ namespace Bundle\PlayWithElasticSearchBundle\Controller;
 
 use Elastica\Client;
 use Elastica\Index;
+use Elastica\Query;
+use Elastica\Query\MatchAll;
+use Elastica\Search;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
@@ -21,6 +24,56 @@ class ElasticaController extends Controller
         $this->elasticaClient = new Client();
     }
 
+    public function searchAction()
+    {
+        $playListIndex = $this->elasticaClient->getIndex('playlist');
+        $trackType     = $playListIndex->getType('track');
+        $search        = new Search($this->elasticaClient);
+
+        $search
+            ->addIndex($playListIndex)
+            ->addType($trackType);
+
+        $query = new Query();
+
+        $query
+            ->setSize(5)
+            ->setSort(['name' => 'asc'])
+            ->setFields(['name', 'ids', 'id', 'composer'])
+            ->setExplain(true)
+            ->setVersion(true)
+            ->setHighlight(['fields' => 'composer'])
+//            ->setSource(['obj1.*', 'obj2.'])
+//            ->setFrom(50)
+//            ->setMinScore(0.5)
+        ;
+
+        $query->setQuery(new MatchAll());
+
+//        $query->addAggregation(new \Elastica\Aggregation\Range('name'));
+//        $term = new \Elastica\Suggest\Term('name', 'field');
+//        $term->setText('aaaaa');
+//        $query->setSuggest(new \Elastica\Suggest($term));
+//        $query->setFacets([new \Elastica\Facet\Range('name')]);
+
+        $search->setQuery($query);
+
+        $resultSet       = $search->search();
+        $numberOfEntries = $search->count();
+        $results         = $resultSet->getResults();
+        $totalResults    = $resultSet->getTotalHits();
+        
+        return $this->render(
+            'PlayWithElasticSearchBundle:Elastica:search.html.twig',
+            [
+                'query'           => $query,
+                'numberOfEntries' => $numberOfEntries,
+                'resultSet'       => $resultSet,
+                'results'         => $results,
+                'totalResults'    => $totalResults
+            ]
+        );
+    }
 
     public function addDocumentAction()
     {
